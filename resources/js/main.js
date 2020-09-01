b11c43554a26a16c6c9f786d4e10e45809ec25e7
Alvi1916039 <69807547+Alvi1916039@users.noreply.github.com>
2020-09-01 17:45:28 +0800

    added html ,css and js files for psd

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+//    bx slider   
+
+$(document).ready(function () {
+    $('.slider-content').bxSlider();
+    $('#main-menu').meanmenu({
+        meanMenuContainer: '#mobile-menu',
+        meanScreenWidth: 991,
+    });
+});
+
